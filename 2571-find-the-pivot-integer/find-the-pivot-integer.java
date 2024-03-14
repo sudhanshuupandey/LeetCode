ad3e3ef84ff445e583f7b3779381c8904499f754
Sudhanshu Pandey <sudhanshupandey257@gmail.com>
2024-03-14 10:47:45 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (94.21%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.java
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.java
@@ -1,18 +1,8 @@
 class Solution {
     public int pivotInteger(int n) {
-        int leftBound = 1, rightBound = n;
-        int fullSum = (n * (n + 1)) / 2;
-        while (leftBound <= rightBound) {
-            int midNum = leftBound + (rightBound - leftBound) / 2;
-            int firstHlfSum = (midNum * (midNum + 1)) / 2;
-            int secondHlfSum = fullSum - firstHlfSum + midNum;
-            if (firstHlfSum == secondHlfSum)
-                return midNum;
-            else if (firstHlfSum < secondHlfSum)
-                leftBound = midNum + 1;
-            else
-                rightBound = midNum - 1;
-        }
+        int ans = (n*(n+1))/2;
+        double a = Math.sqrt(ans);
+        if(a-Math.ceil(a)==0) return (int)a;
         return -1;
     }
 }
